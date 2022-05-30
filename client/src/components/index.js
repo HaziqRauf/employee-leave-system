98ee983cdce1e3cd299890cfd49c42b752fd0051
HaziqRauf <muhammadhaziqrauf@gmail.com>
2022-05-30 16:56:51 +0800

    first commit, changes the icon and some titles and links

---
+++ client/src/components/index.js
@@ -0,0 +1,27 @@
+import Logo from '../components/Logo';
+import FormRow from '../components/FormRow';
+import FormRowSelect from '../components/FormRowSelect';
+import Alert from '../components/Alert';
+import Navbar from '../components/Navbar';
+import SmallSidebar from '../components/SmallSidebar';
+import BigSidebar from '../components/BigSidebar';
+import SearchContainer from '../components/SearchContainer';
+import Loading from '../components/Loading';
+import JobsContainer from '../components/JobsContainer';
+import StatsContainer from '../components/StatsContainer';
+import ChartsContainer from '../components/ChartsContainer';
+
+export {
+  Logo,
+  FormRow,
+  Alert, 
+  Navbar, 
+  BigSidebar,
+  SmallSidebar, 
+  FormRowSelect,
+  SearchContainer,
+  Loading,
+  JobsContainer,
+  StatsContainer,
+  ChartsContainer,
+}
